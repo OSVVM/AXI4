5b939551383fe2fb3a84001a1442095739ab6f52
JimLewis <jim@synthworks.com>
2019-05-06 19:06:49 -0700

    updated comments

--- Axi4Lite/src/Axi4LiteMaster.vhd
+++ Axi4Lite/src/Axi4LiteMaster.vhd
@@ -23,7 +23,7 @@
 --    04/2018    2018.04    First Release
 --
 --
--- Copyright 2017-2108 SynthWorks Design Inc
+-- Copyright 2017-2018 SynthWorks Design Inc
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.

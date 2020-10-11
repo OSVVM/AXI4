53b71f6f166a6d966b5283ce3e29ee0f8f812995
Jim Lewis <jim@synthworks.com>
2020-10-10 23:07:20 -0700

    Adding to test suite

---
+++ AxiStream/testbench/TbStream_SendGetAsync1.vhd
@@ -0,0 +1,224 @@
+--
+--  File Name:         TbStream_SendGetAsync1.vhd
+--  Design Unit Name:  Architecture of TestCtrl
+--  Revision:          OSVVM MODELS STANDARD VERSION
+--
+--  Maintainer:        Jim Lewis      email:  jim@synthworks.com
+--  Contributor(s):
+--     Jim Lewis      jim@synthworks.com
+--
+--
+--  Description:
+--      Validates Stream Model Independent Transactions
+--      Send, Get, Check, 
+--      WaitForTransaction, GetTransactionCount
+--      GetAlertLogID, GetErrorCount, 
+--
+--
+--  Developed by:
+--        SynthWorks Design Inc.
+--        VHDL Training Classes
+--        http://www.SynthWorks.com
+--
+--  Revision History:
+--    Date      Version    Description
+--    05/2017   2018.05    Initial revision
+--    01/2020   2020.01    Updated license notice
+--    10/2020   2020.10    Updated test to include Check, ...
+--
+--
+--  This file is part of OSVVM.
+--  
+--  Copyright (c) 2018 - 2020 by SynthWorks Design Inc.  
+--  
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--  
+--      https://www.apache.org/licenses/LICENSE-2.0
+--  
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--  
+architecture SendGetAsync1 of TestCtrl is
+
+  signal   TestDone : integer_barrier := 1 ;
+  
+  constant DATA_WIDTH : integer := StreamTransmitterTransRec.DataToModel'length ; 
+  constant DATA_BYTES : integer := DATA_WIDTH/8 ; 
+ 
+begin
+
+  ------------------------------------------------------------
+  -- ControlProc
+  --   Set up AlertLog and wait for end of test
+  ------------------------------------------------------------
+  ControlProc : process
+  begin
+    -- Initialization of test
+    SetAlertLogName("TbStream_SendGetAsync1") ;
+    SetLogEnable(PASSED, TRUE) ;    -- Enable PASSED logs
+    SetLogEnable(INFO, TRUE) ;    -- Enable INFO logs
+
+    -- Wait for testbench initialization 
+    wait for 0 ns ;  wait for 0 ns ;
+    TranscriptOpen("./results/TbStream_SendGetAsync1.txt") ;
+    SetTranscriptMirror(TRUE) ; 
+
+    -- Wait for Design Reset
+    wait until nReset = '1' ;  
+    ClearAlerts ;
+
+    -- Wait for test to finish
+    WaitForBarrier(TestDone, 35 ms) ;
+    AlertIf(now >= 35 ms, "Test finished due to timeout") ;
+    AlertIf(GetAffirmCount < 1, "Test is not Self-Checking");
+    
+    TranscriptClose ; 
+--    AlertIfDiff("./results/TbStream_SendGetAsync1.txt", "../sim_shared/validated_results/TbStream_SendGetAsync1.txt", "") ; 
+    
+    print("") ;
+    -- Expecting two check errors at 128 and 256
+    ReportAlerts(ExternalErrors => (0, -2, 0)) ; 
+    print("") ;
+    std.env.stop ; 
+    wait ; 
+  end process ControlProc ; 
+
+  
+  ------------------------------------------------------------
+  -- AxiTransmitterProc
+  --   Generate transactions for AxiTransmitter
+  ------------------------------------------------------------
+  AxiTransmitterProc : process
+    variable Data : std_logic_vector(DATA_WIDTH-1 downto 0) ;
+    variable OffSet : integer ; 
+    variable TransactionCount : integer; 
+    variable ErrorCount : integer; 
+    variable CurTime : time ; 
+    variable TxAlertLogID : AlertLogIDType ; 
+  begin
+    wait until nReset = '1' ;  
+    WaitForClock(StreamTransmitterTransRec, 2) ; 
+    
+    log("Send 256 words with each byte incrementing") ;
+    for i in 1 to 256 loop 
+      -- Create words one byte at a time
+      OffSet := i * DATA_BYTES ;
+      for j in 0 to DATA_BYTES loop 
+        Data := to_slv((OffSet + j) mod 256, 8) & Data(Data'left downto 8) ;
+      end loop ; 
+      SendAsync(StreamTransmitterTransRec, Data) ;
+      GetTransactionCount(StreamTransmitterTransRec, TransactionCount) ;
+      log("Transmitter Queued Transactions: " & to_string(i) & " Completed Transactions: " & to_string(TransactionCount)) ;
+--      AffirmIfEqual(TransactionCount, i, "Transmit Queued Transactions:  TransactionCount:") ;
+      if i mod 2 = 0 then 
+        GetErrorCount(StreamTransmitterTransRec, ErrorCount) ;
+        AffirmIfEqual(ErrorCount, 0, "Transmitter, GetErrorCount: Verify that ErrorCount is 0") ;
+      else
+        GetAlertLogID(StreamTransmitterTransRec, TxAlertLogID) ;
+        ErrorCount := GetAlertCount(TxAlertLogID) ; 
+        AffirmIfEqual(ErrorCount, 0, "Transmitter, GetAlertLogID/GetAlertCount: Verify that ErrorCount is 0") ;
+      end if ; 
+      if (i mod 32) = 0 then
+        -- Verify that no transactions are pending
+        CurTime := now ; 
+        WaitForTransaction(StreamTransmitterTransRec) ;
+        Log("Transmitter: WaitForTransaction started at: " & to_string(CurTime, 1 ns) & "  finished at: " & to_string(now, 1 ns)) ;
+--        AffirmIfEqual(now, CurTime, "Transmitter: WaitForTransaction executes in 0 time when using blocking transactions") ;
+      end if ; 
+    end loop ;
+   
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(StreamTransmitterTransRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process AxiTransmitterProc ;
+
+
+  ------------------------------------------------------------
+  -- AxiReceiverProc
+  --   Generate transactions for AxiReceiver
+  ------------------------------------------------------------
+  AxiReceiverProc : process
+    variable ExpData, RxData : std_logic_vector(DATA_WIDTH-1 downto 0) ;  
+    variable OffSet : integer ; 
+    variable TransactionCount : integer ;     
+    variable ErrorCount : integer; 
+    variable CurTime : time ; 
+    variable TxAlertLogID : AlertLogIDType ; 
+    variable Available : boolean ; 
+  begin
+    WaitForClock(StreamReceiverTransRec, 2) ; 
+    
+    -- Get and check the 256 words
+    log("Send 256 words with each byte incrementing") ;
+    for i in 1 to 256 loop 
+      -- Create words one byte at a time
+      OffSet := i * DATA_BYTES ;
+      for j in 0 to DATA_BYTES loop 
+        ExpData := to_slv((OffSet + j) mod 256, 8) & ExpData(ExpData'left downto 8) ;
+      end loop ; 
+      -- Alternate using Get and Check
+      if (i mod 2) /= 0 then 
+        loop 
+          TryGet(StreamReceiverTransRec, RxData, Available) ; 
+          exit when Available ; 
+          WaitForClock(StreamReceiverTransRec, 1) ; 
+        end loop ; 
+        GetTransactionCount(StreamReceiverTransRec, TransactionCount) ;
+        AffirmIfEqual(TransactionCount, i, "Receive TranasctionCount:") ;
+        AffirmIfEqual(RxData, ExpData, "Get: ") ;
+      else 
+        -- Create two check failures
+        if (i mod 128) /= 0 then 
+          loop 
+            TryCheck(StreamReceiverTransRec, ExpData, Available) ; 
+            exit when Available ; 
+            WaitForClock(StreamReceiverTransRec, 1) ; 
+          end loop ; 
+        else
+          -- Create error on model side
+          loop 
+            TryCheck(StreamReceiverTransRec, ExpData+1, Available) ; 
+            exit when Available ; 
+            WaitForClock(StreamReceiverTransRec, 1) ; 
+          end loop ; 
+        end if ; 
+        GetTransactionCount(StreamReceiverTransRec, TransactionCount) ;
+        AffirmIfEqual(TransactionCount, i, "Receive TranasctionCount:") ;
+      end if ; 
+      if i mod 2 = 0 then 
+        GetErrorCount(StreamReceiverTransRec, ErrorCount) ;
+        AffirmIfEqual(ErrorCount, i/128, "Transmitter, GetErrorCount: Verify that ErrorCount is 0") ;
+      else
+        GetAlertLogID(StreamReceiverTransRec, TxAlertLogID) ;
+        ErrorCount := GetAlertCount(TxAlertLogID) ; 
+        AffirmIfEqual(ErrorCount, i/128, "Transmitter, GetAlertLogID/GetAlertCount: Verify that ErrorCount is 0") ;
+      end if ; 
+      if (i mod 32) = 0 then
+        -- Verify that no transactions are pending
+        CurTime := now ; 
+        WaitForTransaction(StreamReceiverTransRec) ;
+        AffirmIfEqual(now, CurTime, "Receiver: WaitForTransaction executes in 0 time when using blocking transactions") ;
+      end if ; 
+     end loop ;
+     
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(StreamReceiverTransRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process AxiReceiverProc ;
+
+end SendGetAsync1 ;
+
+Configuration TbStream_SendGetAsync1 of TbStream is
+  for TestHarness
+    for TestCtrl_1 : TestCtrl
+      use entity work.TestCtrl(SendGetAsync1) ; 
+    end for ; 
+  end for ; 
+end TbStream_SendGetAsync1 ; 
---
+++ AxiStream/testbench/TbStream_SendGetBurst1.vhd
@@ -0,0 +1,165 @@
+--
+--  File Name:         TbStream_SendGetBurst1.vhd
+--  Design Unit Name:  Architecture of TestCtrl
+--  Revision:          OSVVM MODELS STANDARD VERSION
+--
+--  Maintainer:        Jim Lewis      email:  jim@synthworks.com
+--  Contributor(s):
+--     Jim Lewis      jim@synthworks.com
+--
+--
+--  Description:
+--      Validates Stream Model Independent Transactions
+--      SendBurst, GetBurst
+--
+--
+--  Developed by:
+--        SynthWorks Design Inc.
+--        VHDL Training Classes
+--        http://www.SynthWorks.com
+--
+--  Revision History:
+--    Date      Version    Description
+--    05/2017   2018.05    Initial revision
+--    01/2020   2020.01    Updated license notice
+--    10/2020   2020.10    Updated test to include Check, ...
+--
+--
+--  This file is part of OSVVM.
+--  
+--  Copyright (c) 2018 - 2020 by SynthWorks Design Inc.  
+--  
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--  
+--      https://www.apache.org/licenses/LICENSE-2.0
+--  
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--  
+architecture SendGetBurst1 of TestCtrl is
+
+  signal   TestDone : integer_barrier := 1 ;
+  
+  constant DATA_WIDTH : integer := StreamTransmitterTransRec.DataToModel'length ; 
+  constant DATA_BYTES : integer := DATA_WIDTH/8 ; 
+  
+  alias TxBurstFifo is <<variable .TbStream.AxiStreamTransmitter_1.BurstFifo : osvvm.ScoreboardPkg_slv.ScoreboardPType>> ;
+  alias RxBurstFifo is <<variable .TbStream.AxiStreamReceiver_1.BurstFifo : osvvm.ScoreboardPkg_slv.ScoreboardPType>> ;
+ 
+begin
+
+  ------------------------------------------------------------
+  -- ControlProc
+  --   Set up AlertLog and wait for end of test
+  ------------------------------------------------------------
+  ControlProc : process
+  begin
+    -- Initialization of test
+    SetAlertLogName("TbStream_SendGetBurst1") ;
+    SetLogEnable(PASSED, TRUE) ;    -- Enable PASSED logs
+    SetLogEnable(INFO, TRUE) ;    -- Enable INFO logs
+
+    -- Wait for testbench initialization 
+    wait for 0 ns ;  wait for 0 ns ;
+    TranscriptOpen("./results/TbStream_SendGetBurst1.txt") ;
+    SetTranscriptMirror(TRUE) ; 
+
+    -- Wait for Design Reset
+    wait until nReset = '1' ;  
+    ClearAlerts ;
+
+    -- Wait for test to finish
+    WaitForBarrier(TestDone, 5 ms) ;
+    AlertIf(now >= 5 ms, "Test finished due to timeout") ;
+    AlertIf(GetAffirmCount < 1, "Test is not Self-Checking");
+    
+    TranscriptClose ; 
+--    AlertIfDiff("./results/TbStream_SendGetBurst1.txt", "../sim_shared/validated_results/TbStream_SendGetBurst1.txt", "") ; 
+    
+    print("") ;
+    -- Expecting two check errors at 128 and 256
+    ReportAlerts ; 
+    print("") ;
+    std.env.stop ; 
+    wait ; 
+  end process ControlProc ; 
+
+  
+  ------------------------------------------------------------
+  -- AxiTransmitterProc
+  --   Generate transactions for AxiTransmitter
+  ------------------------------------------------------------
+  AxiTransmitterProc : process
+  begin
+    wait until nReset = '1' ;  
+    WaitForClock(StreamTransmitterTransRec, 2) ; 
+    
+    log("Transmit 32 Bytes -- word aligned") ;
+    PushBurstIncrement(TxBurstFifo, 3, 32) ;
+    SendBurst(StreamTransmitterTransRec, 32) ;
+
+    WaitForClock(StreamTransmitterTransRec, 4) ; 
+
+    log("Transmit 30 Bytes -- unaligned") ;
+    PushBurst(TxBurstFifo, (1,3,5,7,9,11,13,15,17,19,21,23,25,27,29)) ;
+    PushBurst(TxBurstFifo, (31,33,35,37,39,41,43,45,47,49,51,53,55,57,59)) ;
+    SendBurst(StreamTransmitterTransRec, 30) ;
+
+    WaitForClock(StreamTransmitterTransRec, 4) ; 
+
+    log("Transmit 34 Bytes -- unaligned") ;
+    PushBurstRandom(TxBurstFifo, 7, 34) ;
+    SendBurst(StreamTransmitterTransRec, 34) ;
+
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(StreamTransmitterTransRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process AxiTransmitterProc ;
+
+
+  ------------------------------------------------------------
+  -- AxiReceiverProc
+  --   Generate transactions for AxiReceiver
+  ------------------------------------------------------------
+  AxiReceiverProc : process
+    variable NumBytes : integer ; 
+  begin
+    WaitForClock(StreamReceiverTransRec, 2) ; 
+    
+--    log("Transmit 32 Bytes -- word aligned") ;
+    GetBurst (StreamReceiverTransRec, NumBytes) ;
+    AffirmIfEqual(NumBytes, 32, "Receiver: NumBytes Received") ;
+    CheckBurstIncrement(RxBurstFifo, 3, NumBytes) ;
+    
+--    log("Transmit 30 Bytes -- unaligned") ;
+    GetBurst (StreamReceiverTransRec, NumBytes) ;
+    AffirmIfEqual(NumBytes, 30, "Receiver: NumBytes Received") ;
+    CheckBurst(RxBurstFifo, (1,3,5,7,9,11,13,15,17,19,21,23,25,27,29)) ;
+    CheckBurst(RxBurstFifo, (31,33,35,37,39,41,43,45,47,49,51,53,55,57,59)) ;
+
+--    log("Transmit 34 Bytes -- unaligned") ;
+    GetBurst (StreamReceiverTransRec, NumBytes) ;
+    AffirmIfEqual(NumBytes, 34, "Receiver: NumBytes Received") ;
+    CheckBurstRandom(RxBurstFifo, 7, NumBytes) ;
+     
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(StreamReceiverTransRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process AxiReceiverProc ;
+
+end SendGetBurst1 ;
+
+Configuration TbStream_SendGetBurst1 of TbStream is
+  for TestHarness
+    for TestCtrl_1 : TestCtrl
+      use entity work.TestCtrl(SendGetBurst1) ; 
+    end for ; 
+  end for ; 
+end TbStream_SendGetBurst1 ; 

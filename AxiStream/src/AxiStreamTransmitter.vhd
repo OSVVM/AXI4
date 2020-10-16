7647f4d6e1c0030065111596236cf18dec95df5c
Jim Lewis <jim@synthworks.com>
2020-10-15 19:54:06 -0700

    Moved UpdateOptions to StreamTbPkg.

--- AxiStream/src/AxiStreamTransmitter.vhd
+++ AxiStream/src/AxiStreamTransmitter.vhd
@@ -55,8 +55,9 @@ library osvvm ;
 library osvvm_common ;
   context osvvm_common.OsvvmCommonContext ;
 
-  use work.AxiStreamOptionsTypePkg.all ; 
+  use work.AxiStreamOptionsPkg.all ; 
   use work.Axi4CommonPkg.all ; 
+  use work.AxiStreamTbPkg.all ;
 
 entity AxiStreamTransmitter is
   generic (
@@ -149,51 +150,10 @@ begin
   --    Dispatches transactions to
   ------------------------------------------------------------
   TransactionDispatcher : process
-    constant ID_LEN       : integer := TID'length ;
-    constant DEST_LEN     : integer := TDest'length ;
-    constant USER_LEN     : integer := TUser'length ;
-    constant PARAM_LEN : integer := ID_LEN + DEST_LEN + USER_LEN + 1 ; 
     variable Data : std_logic_vector(TData'range) ; 
-    variable Param : std_logic_vector(PARAM_LEN-1 downto 0) ;
---    alias Operation : StreamOperationType is TransRec.Operation ;
+    variable Param : std_logic_vector(TransRec.ParamToModel'length-1 downto 0) ;
     variable BytesToSend, NumberTransfers : integer ; 
     variable PopValid : boolean ; 
-    
-    ------------------------------------------------------------
-    impure function UpdateOptions (Param : std_logic_vector(PARAM_LEN-1 downto 0)) return std_logic_vector is
-    ------------------------------------------------------------
-      variable ResultParam : std_logic_vector(Param'range) ; 
-      alias ID    : std_logic_vector(ID_LEN-1 downto 0) is ResultParam(PARAM_LEN-1 downto PARAM_LEN-ID_LEN) ;
-      alias Dest  : std_logic_vector(DEST_LEN-1 downto 0) is ResultParam(DEST_LEN-1 + USER_LEN+1 downto USER_LEN+1) ;
-      alias User  : std_logic_vector(USER_LEN-1 downto 0) is ResultParam(USER_LEN downto 1) ;
-      alias Last  : std_logic is ResultParam(0) ;
-    begin    
-      ResultParam := Param ;
-      
-      if ID'Length > 0 and ID(ID'right) = '-' then
-        ID := ParamID ; 
-      end if ; 
-      
-      if Dest'length > 0 and Dest(Dest'right) = '-' then 
-        Dest := ParamDest ; 
-      end if ; 
-      
-      if User'length > 0 and User(User'right) = '-' then 
-        User := ParamUser ; 
-      end if ; 
-      
-      -- Calculate Last.  
-      if Last = '-' then  -- use defaults
-        if ParamLast <= 1 then 
-          Last := '1' when ParamLast = 1 else '0' ; 
-        else 
-          -- generate last once every ParamLast cycles
-          Last := '1' when ((TransmitRequestCount+1) - LastOffsetCount) mod ParamLast = 0 else '0' ; 
-        end if ; 
-      end if ; 
-      return ResultParam ; 
-    end function UpdateOptions ; 
-    
   begin
     WaitForTransaction(
        Clk      => Clk,
@@ -222,7 +182,14 @@ begin
 
       when SEND | SEND_ASYNC =>
         Data   := FromTransaction(TransRec.DataToModel, Data'length) ;
-        Param  := UpdateOptions(FromTransaction(TransRec.ParamToModel, Param'length)) ;
+        Param  := UpdateOptions(
+                    Param      => FromTransaction(TransRec.ParamToModel, TransRec.ParamToModel'length),
+                    ParamID    => ParamID, 
+                    ParamDest  => ParamDest,
+                    ParamUser  => ParamUser,
+                    ParamLast  => ParamLast,           
+                    Count      => ((TransmitRequestCount+1) - LastOffsetCount)
+                  ) ;
         TransmitFifo.Push(Data & Param) ; 
         Increment(TransmitRequestCount) ;
         wait for 0 ns ; 
@@ -234,7 +201,14 @@ begin
         BytesToSend := TransRec.IntToModel ;
         NumberTransfers := integer(ceil(real(BytesToSend) / real(AXI_STREAM_DATA_BYTE_WIDTH))) ;
         TransmitRequestCount <= TransmitRequestCount + NumberTransfers ; 
-        Param  := UpdateOptions(FromTransaction(TransRec.ParamToModel, Param'length)) ;
+        Param  := UpdateOptions(
+                    Param      => FromTransaction(TransRec.ParamToModel, TransRec.ParamToModel'length),
+                    ParamID    => ParamID, 
+                    ParamDest  => ParamDest,
+                    ParamUser  => ParamUser,
+                    ParamLast  => ParamLast,           
+                    Count      => ((TransmitRequestCount+1) - LastOffsetCount)
+                  ) ;
         while BytesToSend > 0 loop
           PopWord(BurstFifo, PopValid, Data, BytesToSend) ; 
           AlertIfNot(ModelID, PopValid, "BurstFifo Empty during burst transfer", FAILURE) ; 
@@ -398,6 +372,7 @@ begin
 
       -- Signal completion
       Increment(TransmitDoneCount) ;
+      wait for 0 ns ; 
     end loop TransmitLoop ; 
   end process TransmitHandler ;
 

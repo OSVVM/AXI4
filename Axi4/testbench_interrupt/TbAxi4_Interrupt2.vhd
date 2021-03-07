750280859ee0020bae81a8e235b28e1221143f6b
Jim Lewis <jim@synthworks.com>
2021-03-07 14:29:19 -0800

    tests for interrupt handler

---
+++ Axi4/testbench_interrupt/TbAxi4_Interrupt2.vhd
@@ -0,0 +1,187 @@
+--
+--  File Name:         TbAxi4_Interrupt2.vhd
+--  Design Unit Name:  Architecture of TestCtrl
+--  Revision:          OSVVM MODELS STANDARD VERSION
+--
+--  Maintainer:        Jim Lewis      email:  jim@synthworks.com
+--  Contributor(s):
+--     Jim Lewis      jim@synthworks.com
+--
+--
+--  Description:
+--      Test transaction source
+--
+--
+--  Developed by:
+--        SynthWorks Design Inc.
+--        VHDL Training Classes
+--        http://www.SynthWorks.com
+--
+--  Revision History:
+--    Date      Version    Description
+--    04/202`   202`.04    Initial revision
+--
+--
+--  This file is part of OSVVM.
+--  
+--  Copyright (c) 2021 by SynthWorks Design Inc.  
+--  
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--  
+--      https://www.apache.org/licenses/LICENSE-2.0
+--  
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--  
+
+architecture Interrupt2 of TestCtrl is
+
+  signal MasterSync1, MemorySync1, TestDone : integer_barrier := 1 ;
+ 
+begin
+
+  ------------------------------------------------------------
+  -- ControlProc
+  --   Set up AlertLog and wait for end of test
+  ------------------------------------------------------------
+  ControlProc : process
+  begin
+    -- Initialization of test
+    SetAlertLogName("TbAxi4_Interrupt2") ;
+    SetLogEnable(PASSED, TRUE) ;    -- Enable PASSED logs
+    SetLogEnable(INFO, TRUE) ;    -- Enable INFO logs
+    SetLogEnable(GetAlertLogID("Memory_1"), INFO, FALSE) ;   
+
+    -- Wait for testbench initialization 
+    wait for 0 ns ;  wait for 0 ns ;
+    TranscriptOpen("./results/TbAxi4_Interrupt2.txt") ;
+    SetTranscriptMirror(TRUE) ; 
+
+    -- Wait for Design Reset
+    wait until nReset = '1' ;  
+    ClearAlerts ;
+
+    -- Wait for test to finish
+    WaitForBarrier(TestDone, 35 ms) ;
+    AlertIf(now >= 35 ms, "Test finished due to timeout") ;
+    AlertIf(GetAffirmCount < 1, "Test is not Self-Checking");
+    
+    
+    TranscriptClose ; 
+    -- Printing differs in different simulators due to differences in process order execution
+    -- AlertIfDiff("./results/TbAxi4_Interrupt2.txt", "../AXI4/Axi4/testbench/validated_results/TbAxi4_Interrupt2.txt", "") ; 
+    
+    print("") ;
+    ReportAlerts ; 
+    print("") ;
+    std.env.stop ; 
+    wait ; 
+  end process ControlProc ; 
+
+  ------------------------------------------------------------
+  -- MasterProc
+  --   Generate transactions for AxiMaster
+  ------------------------------------------------------------
+  MasterProc : process
+    variable Data : std_logic_vector(AXI_DATA_WIDTH-1 downto 0) := (others => '0') ;    
+  begin
+    wait until nReset = '1' ;  
+    WaitForClock(MasterRec, 2) ; 
+    
+    for i in 0 to 7 loop 
+      blankline(2) ; 
+      log("Main Starting Writes.  Loop #" & to_string(i)) ;
+      Write(MasterRec, X"1000_0000", Data ) ;
+      Write(MasterRec, X"1000_0004", Data + 1 ) ;
+      Write(MasterRec, X"1000_0008", Data + 2 ) ;
+      Write(MasterRec, X"1000_000C", Data + 3 ) ;
+      
+      IntReq <= '1' after i * 10 ns + 5 ns, '0' after i * 10 ns + 50 ns ;  
+      wait for 9 ns ; 
+      Write(MasterRec, X"1000_0010", Data + 4 ) ;
+      ReadCheck(MasterRec, X"1000_0010", Data + 4 ) ;
+      Write(MasterRec, X"1000_0014", Data + 5 ) ;
+      ReadCheck(MasterRec, X"1000_0014", Data + 5 ) ;
+      WaitForClock(MasterRec, 1) ; 
+      log("WaitForClock #1 finished") ;
+      WaitForClock(MasterRec, 1) ; 
+      log("WaitForClock #2 finished") ;
+
+      blankline(2) ; 
+      log("Main Starting Reads.  Loop #" & to_string(i)) ;
+      ReadCheck(MasterRec, X"A000_2000", Data ) ;
+      ReadCheck(MasterRec, X"A000_2004", Data + 1 ) ;
+      ReadCheck(MasterRec, X"A000_2008", Data + 2 ) ;
+      ReadCheck(MasterRec, X"A000_200C", Data + 3 ) ;
+
+      Data := Data + X"10" ;
+    end loop ; 
+
+    
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(MasterRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process MasterProc ;
+
+
+  ------------------------------------------------------------
+  -- InterruptProc
+  --   Generate transactions for AxiResponder
+  ------------------------------------------------------------
+  InterruptProc : process
+    variable Data : std_logic_vector(AXI_DATA_WIDTH-1 downto 0) := (others => '0') ;    
+  begin
+    WaitForClock(InterruptRec, 1) ; 
+    blankline(2) ; 
+    log("Interrupt Handler Started") ; 
+    ReadCheck(InterruptRec, X"1000_0000", Data ) ;
+    ReadCheck(InterruptRec, X"1000_0004", Data + 1 ) ;
+    ReadCheck(InterruptRec, X"1000_0008", Data + 2 ) ;
+    ReadCheck(InterruptRec, X"1000_000C", Data + 3 ) ;
+    
+    Write(InterruptRec, X"A000_2000", Data ) ;
+    Write(InterruptRec, X"A000_2004", Data + 1 ) ;
+    Write(InterruptRec, X"A000_2008", Data + 2 ) ;
+    Write(InterruptRec, X"A000_200C", Data + 3 ) ;
+    
+    Data := Data + X"10" ;
+
+    log("Interrupt Handler Done") ; 
+    blankline(2) ; 
+    InterruptReturn(InterruptRec) ;
+  end process InterruptProc ;
+
+  ------------------------------------------------------------
+  -- ResponderProc
+  --   Generate transactions for AxiResponder
+  ------------------------------------------------------------
+  ResponderProc : process
+    variable Addr : std_logic_vector(AXI_ADDR_WIDTH-1 downto 0) ;
+    variable Data : std_logic_vector(AXI_DATA_WIDTH-1 downto 0) ;    
+  begin
+
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(ResponderRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process ResponderProc ;
+
+
+end Interrupt2 ;
+
+Configuration TbAxi4_Interrupt2 of TbAxi4Memory is
+  for TestHarness
+    for TestCtrl_1 : TestCtrl
+      use entity work.TestCtrl(Interrupt2) ; 
+    end for ; 
+--!!    for Responder_1 : Axi4Responder 
+--!!      use entity OSVVM_AXI4.Axi4Memory ; 
+--!!    end for ; 
+  end for ; 
+end TbAxi4_Interrupt2 ; 
---
+++ Axi4/testbench_interrupt/TbAxi4_InterruptBurst1.vhd
@@ -0,0 +1,179 @@
+--
+--  File Name:         TbAxi4_InterruptBurst1.vhd
+--  Design Unit Name:  Architecture of TestCtrl
+--  Revision:          OSVVM MODELS STANDARD VERSION
+--
+--  Maintainer:        Jim Lewis      email:  jim@synthworks.com
+--  Contributor(s):
+--     Jim Lewis      jim@synthworks.com
+--
+--
+--  Description:
+--      Test transaction source
+--
+--
+--  Developed by:
+--        SynthWorks Design Inc.
+--        VHDL Training Classes
+--        http://www.SynthWorks.com
+--
+--  Revision History:
+--    Date      Version    Description
+--    04/202`   202`.04    Initial revision
+--
+--
+--  This file is part of OSVVM.
+--  
+--  Copyright (c) 2021 by SynthWorks Design Inc.  
+--  
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--  
+--      https://www.apache.org/licenses/LICENSE-2.0
+--  
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--  
+
+architecture InterruptBurst1 of TestCtrl is
+
+  signal MasterSync1, MemorySync1, TestDone : integer_barrier := 1 ;
+ 
+begin
+
+  ------------------------------------------------------------
+  -- ControlProc
+  --   Set up AlertLog and wait for end of test
+  ------------------------------------------------------------
+  ControlProc : process
+  begin
+    -- Initialization of test
+    SetAlertLogName("TbAxi4_InterruptBurst1") ;
+    SetLogEnable(PASSED, TRUE) ;    -- Enable PASSED logs
+    SetLogEnable(INFO, TRUE) ;    -- Enable INFO logs
+    SetLogEnable(GetAlertLogID("Memory_1"), INFO, FALSE) ;   
+
+    -- Wait for testbench initialization 
+    wait for 0 ns ;  wait for 0 ns ;
+    TranscriptOpen("./results/TbAxi4_InterruptBurst1.txt") ;
+    SetTranscriptMirror(TRUE) ; 
+
+    -- Wait for Design Reset
+    wait until nReset = '1' ;  
+    ClearAlerts ;
+
+    -- Wait for test to finish
+    WaitForBarrier(TestDone, 35 ms) ;
+    AlertIf(now >= 35 ms, "Test finished due to timeout") ;
+    AlertIf(GetAffirmCount < 1, "Test is not Self-Checking");
+    
+    
+    TranscriptClose ; 
+    -- Printing differs in different simulators due to differences in process order execution
+    -- AlertIfDiff("./results/TbAxi4_InterruptBurst1.txt", "../AXI4/Axi4/testbench/validated_results/TbAxi4_InterruptBurst1.txt", "") ; 
+    
+    print("") ;
+    ReportAlerts ; 
+    print("") ;
+    std.env.stop ; 
+    wait ; 
+  end process ControlProc ; 
+
+  ------------------------------------------------------------
+  -- MasterProc
+  --   Generate transactions for AxiMaster
+  ------------------------------------------------------------
+  MasterProc : process
+    variable Data : integer := 0 ;    
+  begin
+    wait until nReset = '1' ;  
+    WaitForClock(MasterRec, 2) ; 
+    
+    for i in 0 to 3 loop 
+      blankline(2) ; 
+      log("Main Starting Writes.  Loop #" & to_string(i)) ;
+      PushBurstIncrement(WriteBurstFifo, Data, 4, AXI_DATA_WIDTH) ;
+      WriteBurst(MasterRec, X"1000_0000", 4) ;
+      
+      IntReq <= '1' after i * 10 ns + 5 ns, '0' after i * 10 ns + 50 ns ;  
+      wait for 9 ns ; 
+      WaitForClock(MasterRec, 1) ; 
+      log("WaitForClock #1 finished") ;
+      WaitForClock(MasterRec, 1) ; 
+      log("WaitForClock #2 finished") ;
+      WaitForClock(MasterRec, 1) ; 
+      log("WaitForClock #3 finished") ;
+      WaitForClock(MasterRec, 1) ; 
+      log("WaitForClock #4 finished") ;
+
+      blankline(2) ; 
+      log("Main Starting Reads.  Loop #" & to_string(i)) ;
+      ReadBurst(MasterRec, X"A000_2000", 4) ;
+      CheckBurstIncrement(ReadBurstFifo, Data+16, 4, AXI_DATA_WIDTH) ; 
+
+      Data := Data + 16#10# ;
+    end loop ; 
+
+    
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(MasterRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process MasterProc ;
+
+
+  ------------------------------------------------------------
+  -- InterruptProc
+  --   Generate transactions for AxiResponder
+  ------------------------------------------------------------
+  InterruptProc : process
+    variable Data : integer := 0 ;    
+  begin
+    WaitForClock(InterruptRec, 1) ; 
+    blankline(2) ; 
+    log("Interrupt Handler Started") ; 
+    ReadBurst(InterruptRec, X"1000_0000", 4) ;
+    CheckBurstIncrement(ReadBurstFifo, Data, 4, AXI_DATA_WIDTH) ;
+    
+    PushBurstIncrement(WriteBurstFifo, Data+16, 4, AXI_DATA_WIDTH) ;
+    WriteBurst(InterruptRec, X"A000_2000", 4) ;
+    
+    Data := Data + 16#10# ;
+
+    log("Interrupt Handler Done") ; 
+    blankline(2) ; 
+    InterruptReturn(InterruptRec) ;
+  end process InterruptProc ;
+
+  ------------------------------------------------------------
+  -- ResponderProc
+  --   Generate transactions for AxiResponder
+  ------------------------------------------------------------
+  ResponderProc : process
+    variable Addr : std_logic_vector(AXI_ADDR_WIDTH-1 downto 0) ;
+    variable Data : std_logic_vector(AXI_DATA_WIDTH-1 downto 0) ;    
+  begin
+
+    -- Wait for outputs to propagate and signal TestDone
+    WaitForClock(ResponderRec, 2) ;
+    WaitForBarrier(TestDone) ;
+    wait ;
+  end process ResponderProc ;
+
+
+end InterruptBurst1 ;
+
+Configuration TbAxi4_InterruptBurst1 of TbAxi4Memory is
+  for TestHarness
+    for TestCtrl_1 : TestCtrl
+      use entity work.TestCtrl(InterruptBurst1) ; 
+    end for ; 
+--!!    for Responder_1 : Axi4Responder 
+--!!      use entity OSVVM_AXI4.Axi4Memory ; 
+--!!    end for ; 
+  end for ; 
+end TbAxi4_InterruptBurst1 ; 

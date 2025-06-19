1793b6c9a88fc662e1fd59ff610c1dfa586da8f7
Jim Lewis <jim@synthworks.com>
2025-06-19 10:10:10 -0700

    Removed usage of OSVVM_RESULTS_DIR - not needed since the scripts move the Transcript files

--- Axi4/testbench_interrupt/TbAxi4_InterruptBurst1.vhd
+++ Axi4/testbench_interrupt/TbAxi4_InterruptBurst1.vhd
@@ -59,7 +59,7 @@ begin
 
     -- Wait for testbench initialization 
     wait for 0 ns ;  wait for 0 ns ;
-    TranscriptOpen(OSVVM_RESULTS_DIR & "TbAxi4_InterruptBurst1.txt") ;
+    TranscriptOpen("TbAxi4_InterruptBurst1.txt") ;
     SetTranscriptMirror(TRUE) ; 
 
     -- Wait for Design Reset

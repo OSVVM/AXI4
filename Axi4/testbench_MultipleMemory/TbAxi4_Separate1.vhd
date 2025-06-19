49cc90216ea6c63a87a31122572ada1957bb59cb
Jim Lewis <jim@synthworks.com>
2025-06-19 10:09:42 -0700

    Removed usage of OSVVM_RESULTS_DIR - not needed since the scripts move the Transcript files

--- Axi4/testbench_MultipleMemory/TbAxi4_Separate1.vhd
+++ Axi4/testbench_MultipleMemory/TbAxi4_Separate1.vhd
@@ -61,7 +61,7 @@ begin
 
     -- Wait for testbench initialization 
     wait for 0 ns ;  wait for 0 ns ;
-    TranscriptOpen(OSVVM_RESULTS_DIR & "TbAxi4_Separate1.txt") ;
+    TranscriptOpen("TbAxi4_Separate1.txt") ;
     SetTranscriptMirror(TRUE) ; 
 
     -- Wait for Design Reset
@@ -76,7 +76,7 @@ begin
     
     TranscriptClose ; 
     -- Printing differs in different simulators due to differences in process order execution
-    -- AlertIfDiff(OSVVM_RESULTS_DIR & "TbAxi4_Separate1.txt", "../AXI4/Axi4/testbench/validated_results/TbAxi4_Separate1.txt", "") ; 
+    -- AlertIfDiff("TbAxi4_Separate1.txt", "../AXI4/Axi4/testbench/validated_results/TbAxi4_Separate1.txt", "") ; 
 
     EndOfTestReports(ExternalErrors => (0, -12, 0)) ; 
     std.env.stop ; 
--- Axi4/testbench_MultipleMemory/TbAxi4_Shared1.vhd
+++ Axi4/testbench_MultipleMemory/TbAxi4_Shared1.vhd
@@ -61,7 +61,7 @@ begin
 
     -- Wait for testbench initialization 
     wait for 0 ns ;  wait for 0 ns ;
-    TranscriptOpen(OSVVM_RESULTS_DIR & "TbAxi4_Shared1.txt") ;
+    TranscriptOpen("TbAxi4_Shared1.txt") ;
     SetTranscriptMirror(TRUE) ; 
 
     -- Wait for Design Reset
@@ -76,7 +76,7 @@ begin
     
     TranscriptClose ; 
     -- Printing differs in different simulators due to differences in process order execution
-    -- AlertIfDiff(OSVVM_RESULTS_DIR & "TbAxi4_Shared1.txt", "../AXI4/Axi4/testbench/validated_results/TbAxi4_Shared1.txt", "") ; 
+    -- AlertIfDiff("TbAxi4_Shared1.txt", "../AXI4/Axi4/testbench/validated_results/TbAxi4_Shared1.txt", "") ; 
 
     EndOfTestReports ; 
     std.env.stop ; 

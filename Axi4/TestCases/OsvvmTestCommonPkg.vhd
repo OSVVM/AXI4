94aa532502e364746240f9813298270ec91def9e
Jim Lewis <jim@synthworks.com>
2025-06-19 12:17:14 -0700

    Replaced by autogenerated version

--- Axi4/TestCases/OsvvmTestCommonPkg.vhd
+++
@@ -1,4 +0,0 @@
-package OsvvmTestCommonPkg is
-  constant OSVVM_RESULTS_DIR   : string := "" ;
-  constant OSVVM_PATH_TO_TESTS : string := "../../OsvvmLibraries/" ;
-end package OsvvmTestCommonPkg ; 

72e862c56311f3b47c0c1cb54ff3a777c5604e6e
Jim Lewis <jim@synthworks.com>
2025-09-30 16:52:21 -0700

    Support for TestCtrl having mode views on the interface

---
+++ Axi4/testbench/deprecated/TestCtrlComponentPkg_c.vhd
@@ -0,0 +1,66 @@
+--
+--  File Name:         TestCtrlComponentPkg.vhd
+--  Design Unit Name:  TestCtrlComponentPkg
+--  Revision:          OSVVM MODELS STANDARD VERSION
+--
+--  Maintainer:        Jim Lewis      email:  jim@synthworks.com
+--  Contributor(s):
+--     Jim Lewis      jim@synthworks.com
+--
+--
+--  Description:
+--      Test transaction source
+--
+--
+--  Developed by:
+--        SynthWorks Design Inc.
+--        VHDL Training Classes
+--        http://www.SynthWorks.com
+--
+--  Revision History:
+--    Date      Version    Description
+--    10/2025   2025.10    Initial revision
+--
+--
+--  This file is part of OSVVM.
+--  
+--  Copyright (c) 2025 by SynthWorks Design Inc.  
+--  
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--  
+--      https://www.apache.org/licenses/LICENSE-2.0
+--  
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--  
+
+library ieee ;
+  use ieee.std_logic_1164.all ;
+  
+library OSVVM ; 
+  context OSVVM.OsvvmContext ; 
+  use osvvm.ScoreboardPkg_slv.all ;
+
+library OSVVM_AXI4 ;
+  context OSVVM_AXI4.Axi4Context ; 
+
+use work.OsvvmTestCommonPkg.all ;
+
+package TestCtrlComponentPkg is
+  component TestCtrl is
+    port (
+      -- Global Signal Interface
+      nReset           : In    std_logic ;
+
+      -- Transaction Interfaces
+      ManagerRec       : inout AddressBusRecType ;
+      SubordinateRec   : inout AddressBusRecType
+    ) ;
+  end component TestCtrl ;
+  
+end package TestCtrlComponentPkg ;
